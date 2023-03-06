97bac2a35d029a164c0edd5178ca0361c2137bf8
Muhamad Aqmal Maulana <muhamadaqmal13@gmail.com>
2023-03-06 11:17:14 +0700

    Fix error

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import getApp from './app';
-import { Config } from './config';
 
 (async()=> {
-    const config = new Config()
-    if(config.nodeEnv === "PRODUCTION") {
-        dotenv.config({path: "/home/user/env/.env"})
+    if(process.env.NODE_ENV === "PRODUCTION") {
+        dotenv.config({path: "/root/myapp/.env"})
     } else {
         dotenv.config()
     }
 
     try {
         const app: Express = express();
-        const port = config.port || 3000;
+        const port = process.env.PORT || 3000;
         const main = getApp(app)
 
         app.listen(port, () => {
